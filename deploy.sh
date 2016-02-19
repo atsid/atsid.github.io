02f900cda86624f276561270b557ef8d58316a2f
cleach <cleach@harding.edu>
2016-02-19 15:40:49 -0800

    Updating the deployment process to reflect the process in the readme file.

--- deploy.sh
+++ deploy.sh
@@ -6,16 +6,6 @@ rev=$(git rev-parse --short HEAD)
 
 cd dist
 
-git init
-git config user.name "Chris Leach"
-git config user.email "chris.leach@atsid.com"
-
-git remote add upstream "https://$GH_TOKEN@github.com/atsid/atsid.github.io.git"
-git fetch upstream
-git reset upstream/gh-pages
-
-touch .
-
-git add -A .
+git add -A
 git commit -m "rebuild pages at ${rev}"
-git push -q upstream HEAD:gh-pages
+git push origin master
