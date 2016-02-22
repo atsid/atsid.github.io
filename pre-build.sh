823942744146dd47fa11df58513746032d5a05b6
cleach <cleach@harding.edu>
2016-02-22 09:41:30 -0800

    Revert "Updating origin location in pre-build script."
    This reverts commit 8b66902b104568ff940558402f89574aa736318d.

--- pre-build.sh
+++ pre-build.sh
@@ -10,6 +10,6 @@ git init
 git config user.name "Chris Leach"
 git config user.email "chris.leach@atsid.com"
 
-git remote add origin git@github.com:atsid/atsid.github.io.git
+git remote add origin "https://$GH_TOKEN@github.com/atsid/atsid.github.io.git"
 git pull origin travis_temp
 cd ..
